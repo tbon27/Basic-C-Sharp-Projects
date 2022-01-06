f7aa169a83f0867cc67a65123cdb23a119b3479a
Tony B <tbongio27@gmail.com>
2022-01-06 13:01:07 -0700

    Uploaded Basic C# starter files

---
+++ Basic_Demos/C_Sharp_Assignment_1/C_Sharp_Assignment1/C_Sharp_Assignment1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("===========================================");
+            Console.WriteLine("===  Welcome to Acme Accounting Systems");
+            Console.WriteLine("======  Remember, we're \"accounting\" on you!");
+            Console.WriteLine("===========================================");
+            Console.ReadLine();
+        }
+    }
+}
