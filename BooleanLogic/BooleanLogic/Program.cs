1dd029fc9b01ef1a81a0e906c39548a536c0641f
Tony B <tbongio27@gmail.com>
2022-01-10 12:27:10 -0700

    Added Boolean Logic demo files
    Continued coursework - boolean logic and operators

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // AND Operator &&
+            Console.WriteLine(true && false); //false
+            Console.WriteLine(true && true); //true
+            Console.WriteLine(false && false); //false
+
+            // OR Operator ||
+            Console.WriteLine(true || true); //true
+            Console.WriteLine(true || false); //true
+            Console.WriteLine(false || false); //false
+
+            // Equal to operator ==
+            Console.WriteLine(true == true); //true
+            Console.WriteLine(true == false); //false
+            Console.WriteLine(false == false); //true
+
+            //does not equal operator !=
+            Console.WriteLine(true != true); //false
+            Console.WriteLine(true != false); //true
+            Console.WriteLine(false != false); //false
+
+            // XOR operator - if one is true but NOT BOTH
+            Console.WriteLine(true ^ true); //false
+            Console.WriteLine(true ^ false); //true
+            Console.WriteLine(false ^ false); //false
+
+            Console.ReadLine();
+        }
+    }
+}
