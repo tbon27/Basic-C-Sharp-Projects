016ff78cb9e92cf3b7f3753db25107f4d6c79a07
Tony B <tbongio27@gmail.com>
2022-01-11 12:27:29 -0700

    Created Loops demo
    Continued Coursework

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SWITCH STATEMENT and (DO) WHILE LOOP
+            Console.WriteLine("Guess a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again!");
+                        Console.WriteLine("Guess a number: ");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again!");
+                        Console.WriteLine("Guess a number: ");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again!");
+                        Console.WriteLine("Guess a number: ");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed 12. Correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number: ");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+
+            Console.ReadLine();
+        }
+    }
+}
