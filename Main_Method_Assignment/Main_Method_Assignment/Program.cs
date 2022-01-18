c021cf3ef0c34715ee0ed54636d8100f547058df
Tony B <tbongio27@gmail.com>
2022-01-18 11:52:08 -0700

    Updated Main Method Assignment
    Created class for methods used

--- Main_Method_Assignment/Main_Method_Assignment/Program.cs
+++ Main_Method_Assignment/Main_Method_Assignment/Program.cs
@@ -10,35 +10,42 @@ namespace Main_Method_Assignment
     {
         static void Main(string[] args)
         {
-            int finalResult = Math(2); // passing int through math method
+            MathOps operations = new MathOps(); // create new instance of class MathOps to use methods
+
+            int finalResult = operations.Add(2); // passing int through math method
             Console.WriteLine("The result of 2 added to 250 equals: " + finalResult);
             Console.ReadLine();
 
-            finalResult = Math(2.2); // passing double through math method
+            finalResult = operations.Mult(2.2); // passing double through math method
             Console.WriteLine("The result of 2.2 multiplied by 2.25 equals (rounded): " + finalResult);
             Console.ReadLine();
 
-            int result2 = Math("20"); // passing string through math method
+            int result2 = operations.Divide("20"); // passing string through math method
             Console.WriteLine("The result of string \"20\" divided by 2 equals: " + result2);
             Console.ReadLine();
-        }
 
-        public static int Math(int number) //create method requiring one int parameter
+        }
+    }
+
+    public class MathOps
+    {
+        public int Add(int number) //create method requiring one int parameter
         {
             return number + 250; //do calculation when called
-        }  
-        
-        public static int Math(double number) //create method requiring one double parameter
+        }
+
+        public int Mult(double number) //create method requiring one double parameter
         {
             int result = Convert.ToInt32(number * 2.25);
             return result;//do calculation when called
-        }        
-        
-        public static int Math(string myString) //create method requiring one string parameter
+        }
+
+        public int Divide(string myString) //create method requiring one string parameter
         {
             int newInt = Convert.ToInt32(myString); // convert string to int
             int result = newInt / 2; // perform math
             return result;//do calculation when called
         }
     }
+
 }
