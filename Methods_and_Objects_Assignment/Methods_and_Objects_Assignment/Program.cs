6171b58178395d20f9c86aba4efbddf18d9b84b2
Tony B <tbongio27@gmail.com>
2022-01-18 16:40:10 -0700

    Created Methods and Objects Assignment
    Using methods, classes and superclasses

---
+++ Methods_and_Objects_Assignment/Methods_and_Objects_Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(); // instantiate Employee class
+            employee.FirstName = "Sample"; //initialize
+            employee.LastName = "Student"; //initialize
+
+            employee.SayName(); // call superclass method on employee object (Employee inherits Person)
+            Console.ReadLine();
+        }
+    }
+}
