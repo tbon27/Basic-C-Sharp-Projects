a7ccab7bbe3bf373966b97e489e587a94d7f55ba
Tony B <tbongio27@gmail.com>
2022-01-19 12:17:51 -0700

    Update Twenty One Project
    Added interface class example, applied IWalkAway to class TwentyOneGame

---
+++ TwentyOne/TwentyOne/IWalkAway.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    interface IWalkAway //naming conventions for interfaces begin with "I" (ex: "ILimitPurchase", "IEndSession")
+    // .NET Framework does not support multiple inheritance otherwise
+    // .NET Framework supports multiple inheritance with INTERFACE: inherit a base class, then inherit many interfaces
+    // No implementation details with Interface (similar) to abstract
+    // Everything is public in interface, dont need to state public
+    {
+        void WalkAway(Player player);
+    }
+}
--- TwentyOne/TwentyOne/TwentyOneGame.cs
+++ TwentyOne/TwentyOne/TwentyOneGame.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-    public class TwentyOneGame: Game // inherit a class from Game class
+    public class TwentyOneGame: Game, IWalkAway // inherit a class from Game class, and inherit from INTERFACE class IWalkAway
     {
         public override void Play() // override - we will define this method. contract with Game satisfied (abstract method).
         {
@@ -17,6 +17,11 @@ namespace TwentyOne
         {
             Console.WriteLine("21 Players: ");
             base.ListPlayers(); // this is basically ListPlayers from Game class, but we can make changes - VIRTUAL implementation
+        }
+
+        public void WalkAway(Player player)
+        {
+            throw new NotImplementedException(); // just create method, no implementation, do not call method
         }
     }
 }
