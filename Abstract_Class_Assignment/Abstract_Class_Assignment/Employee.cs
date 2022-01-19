4a973fa2077e0ac0e498273dcfeb70f5f34714c8
Tony B <tbongio27@gmail.com>
2022-01-18 18:12:24 -0700

    Created Abstract Class Assignment
    Creating abstract classes, using virtual methods, and inheritance

---
+++ Abstract_Class_Assignment/Abstract_Class_Assignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    class Employee : Person // inherit from person class
+    {
+        public override void SayName() // override the virtual method within abstract class
+        {
+            base.SayName(); // get from virtual method in Person abstract class
+        }
+
+    }
+}
---
+++ Abstract_Class_Assignment/Abstract_Class_Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(); // instantiate Employee class
+            employee.firstName = "Sample"; //initialize
+            employee.lastName = "Student"; //initialize
+
+            employee.SayName(); // call superclass method on employee object (Employee inherits Person and override virtual method)
+            Console.ReadLine();
+        }
+    }
+}
