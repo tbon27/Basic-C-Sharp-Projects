fd98bb0cab33832b827b284534b23b85aefefe6d
Tony B <tbongio27@gmail.com>
2022-01-19 17:14:26 -0700

    Created Operators Assignment
    Overloading == operator to compare objects

---
+++ Operators_Assignment/Operators_Assignment/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    public abstract class Person // create abstract class - not to be an object ever, inherit from
+    {
+        // properties
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int id { get; set; }
+
+        public virtual void SayName() // make virtual so it can be accessed outside abstract class
+        {
+            Console.WriteLine("Name: {0} {1}, ID#: {2}", firstName, lastName, id);
+        }        
+        
+    }
+}
---
+++ Operators_Assignment/Operators_Assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(); // instantiate Employee class
+            employee.firstName = "Janice"; // initialize
+            employee.lastName = "Johnson"; // initialize
+            employee.id = 1001; // initialize employee1 id int value
+                                           
+            Employee employee2 = new Employee(); // instantiate Employee class
+            employee2.firstName = "Edgar"; // initialize
+            employee2.lastName = "Rawlings"; // initialize
+            //employee2.id = 1001; // initialize employee2 id int value
+            employee2.id = 1005; // test value
+
+            Console.WriteLine("Employee List: ");
+            employee.SayName(); // call superclass method on employee object (Employee inherits Person and override virtual method)
+            employee2.SayName(); // call superclass method on employee object (Employee inherits Person and override virtual method)
+            Console.ReadLine();
+
+
+            //==========================
+            //==  Overload "==" operator
+            //==========================
+            //
+            // here we use our overload comparing Employee objects employee and employee2...
+            // ...resulting in employee1id.id == employee2id.id (TRUE/FALSE)            
+            Console.WriteLine("Employee Id's Match: " + (employee == employee2));
+
+            // And (opposite as above) != overloaded.. employee1id.id != employee2id.id (FALSE)
+            Console.WriteLine("Employee Id's Don't Match: " + (employee != employee2));
+
+            Console.ReadLine();
+        }
+    }
+}
