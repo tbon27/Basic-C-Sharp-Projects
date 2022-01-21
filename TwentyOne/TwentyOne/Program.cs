0f81d3b46006d70568310dd08fcfae6487d76058
Tony B <tbongio27@gmail.com>
2022-01-21 14:36:33 -0700

    Updated project: lambda expression demo

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,16 +10,56 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Card card1 = new Card();
-            Card card2 = card1;
-            card1.Face = Face.Eight;
-            card2.Face = Face.King;
-
-            Console.WriteLine(card1.Face); //results in King
-
-
             Deck deck = new Deck(); // instantiate Deck
-            //deck = Shuffle(deck); // shuffling and reassigning the value to deck
+
+//demo
+                                    //Card card1 = new Card();
+                                    //Card card2 = card1;
+                                    //card1.Face = Face.Eight;
+                                    //card2.Face = Face.King;
+
+            //Console.WriteLine(card1.Face); //results in King
+
+
+//==****** LAMBDA DEMO ****** ==// note: difficult to debug
+//==============================//
+//How many aces (are left) in the deck?
+//
+//First approach:
+/*int counter = 0;
+foreach(Card card in deck.Cards)
+{
+    if(card.Face == Face.Ace)
+    {
+        counter++;
+    }
+}
+Console.WriteLine(counter);*/
+
+//Approach #2 using LAMBDA
+//int count = deck.Cards.Count(x => x.Face == Face.Ace); //count all elements(x) where x.Face equals Face.Ace
+//Console.WriteLine(count);
+
+//==****** LAMBDA DEMO 2 ****** ==//
+//==============================//
+//List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList(); // "take this item, and apply this function to each item"
+//foreach (Card card in newList)
+//{
+//    Console.WriteLine(card.Face);
+//}
+
+//==****** LAMBDA DEMO 3 ****** ==//
+//==============================//
+//List<int> numberList = new List<int>() { 10, 30, 70, 100, 105 };
+
+//int sum = numberList.Sum(x => x + 5); //in other words - for each item, add 5 to each item
+//Console.WriteLine(sum);
+//int maxNum = numberList.Max();
+//Console.WriteLine(maxNum);            
+//int minNum = numberList.Min();
+//Console.WriteLine(minNum);
+//int demoNum1 = numberList.Where(x => x > 20).Sum(); //Where() creates a new list (we are then Sum() summing it)
+//Console.WriteLine(demoNum1);
 
             deck.Shuffle(3); 
             // this is for shuffling multiple times
