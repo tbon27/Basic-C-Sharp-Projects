9365a61c8eeb25f1d1d1732affca1e04b6f640c2
Tony B <tbongio27@gmail.com>
2022-01-21 13:57:29 -0700

    Created Struct Assignment
    Create a struct demo

---
+++ Struct_Assignment/Struct_Assignment/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    public struct Number //create struct Number - value type
+    {
+        public int numberStruct { get; set; } //property: int
+    }
+   
+}
---
+++ Struct_Assignment/Struct_Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number(); //create new instance of Number() called number
+            Number number2 = number; //demo: set number to struct type Number named number2
+            number.numberStruct = 2; // set number to 2
+            number2.numberStruct = 7; // set demo number2 to 7
+            Console.WriteLine(number.numberStruct); //using struct, will print 2 - class will print 7 (overwriting 2)
+        }
+
+    }
+}
