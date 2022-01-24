b06bd7a2da4e242d00ddd8fdcc8741ad1dfd8992
Tony B <tbongio27@gmail.com>
2022-01-24 14:05:51 -0700

    Created DateTime Assignment
    using DateTime() manipulate the hours based on user input

---
+++ Assignments/DateTime_Assignment/DateTime_Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Current date and time: " + DateTime.Now); // display current date/time
+            Console.WriteLine("Press enter...");
+            Console.ReadLine();
+            Console.WriteLine("Please enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine()); //get user input, convert to int and set to userInput
+            DateTime inXhours = DateTime.Now.AddHours(userInput); //get current date and time, and add hours using userInput, set this to DateTime type variable inXhours
+            Console.WriteLine("In {0} hours, the date and time will be: " + inXhours, userInput); //display result of userInput added to current hour
+        }
+    }
+}
