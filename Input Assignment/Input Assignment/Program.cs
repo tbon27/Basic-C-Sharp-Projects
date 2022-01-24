6859cad1bf31a4fe52c68f0e1b9c379c11d296cf
Tony B <tbongio27@gmail.com>
2022-01-24 13:46:09 -0700

    Created Input Assignment
    Ask user for number, log this input to log.txt file

---
+++ Input Assignment/Input Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\tbong\OneDrive\Documents\GitHub\Basic-C-Sharp-Projects\Input Assignment\Logs\log.txt", false)) //false creates new files
+            {
+                file.WriteLine(userInput); // write cards to text file
+            }
+            Console.ReadLine();
+        }
+    }
+}
