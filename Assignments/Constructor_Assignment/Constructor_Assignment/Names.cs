9807adf416421b1d7bc13729c276eae74def217c
Tony B <tbongio27@gmail.com>
2022-01-24 16:22:41 -0700

    Created Constructor Assignment
    Create const variable, use keyword var, chain two constructors together

---
+++ Assignments/Constructor_Assignment/Constructor_Assignment/Names.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Assignment
+{
+    public class Names
+    {
+        // Constructor chaining, constructor callchain
+        public Names(string name) : this(name, 18) //assign name to name, and default age 18
+        { } //no need for implementation here, we are reusing the constructor below, in this constructor
+
+        public Names(string name, int age) //constructor taking two params
+        {
+            Name = name;
+            Age = age;
+        }
+
+        //set properties
+        public string Name { get; set; } 
+        public int Age { get; set; }
+    }
+}
