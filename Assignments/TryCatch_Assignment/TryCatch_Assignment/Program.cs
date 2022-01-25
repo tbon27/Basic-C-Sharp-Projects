37536532ca6077b92d04f4bc21ee74f834fad66d
Tony B <tbongio27@gmail.com>
2022-01-24 17:59:41 -0700

    Created Try/Catch Assignment
    Use exceptions to protect program

---
+++ Assignments/TryCatch_Assignment/TryCatch_Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            try
+            {
+                int userInput = Convert.ToInt32(Console.ReadLine()); //get user input
+                if (userInput < 1) throw new ArgumentOutOfRangeException(); //if user input is 0 or negative number, throw specific exception
+                int currentYear = DateTime.Now.Year; //assign current year to int variable
+                int yearBorn = currentYear - userInput; //calc yearBorn
+
+                Console.WriteLine(yearBorn); //display
+            }
+            catch (ArgumentOutOfRangeException) //using specific exception; we want positive numbers
+            {
+                Console.WriteLine("Please enter positive, non-zero numbers...");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception) //general exception
+            {
+                Console.WriteLine("An error occured, please contact your System Administrator");
+                Console.ReadLine();
+                return;
+            }
+            Console.ReadLine();
+        }
+    }
+}
