9d0cf5cb4f594d9acbd01435e96fe72d0ee0e3c9
Tony B <tbongio27@gmail.com>
2022-01-26 14:24:57 -0700

    Created MyWebAppMVC Project
    Beginning demos with MVC Principles

---
+++ Basic_Demos/MyWebAppMVC/MyWebAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string text = "Hello! The date and time is: " + DateTime.Now;
+            System.IO.File.WriteAllText(@"C:\Users\tbong\OneDrive\Documents\GitHub\Basic-C-Sharp-Projects\Basic_Demos\MyWebAppMVC\Logs\log.txt", text);
+
+            Random rnd = new Random(10);
+            int num = rnd.Next();
+
+            ViewBag.RandomNumber = num;
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
