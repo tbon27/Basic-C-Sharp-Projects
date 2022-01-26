f0d274304ecddeec47c857bf9ad47bfdeda06814
Tony B <tbongio27@gmail.com>
2022-01-26 13:28:48 -0700

    Created a Web Forms app - demo
    customized contact page, customized main title

---
+++ Basic_Demos/MyFirstWebApp/MyFirstWebApp/Contact.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstWebApp
+{
+    public partial class Contact : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = TextBox1.Text; //take whatever is in input, and assign to tool label we dragged in
+        }
+    }
+}
