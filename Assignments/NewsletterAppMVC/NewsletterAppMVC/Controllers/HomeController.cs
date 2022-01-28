5283026c1177056193e55b7d687036f2f846c8ef
Tony B <tbongio27@gmail.com>
2022-01-28 13:36:41 -0700

    Updated Project - Form and input field, etc
    Added form and input fields, taking input and sending to SQL database

--- Assignments/NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
+++ Assignments/NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,17 +15,41 @@ namespace NewsletterAppMVC.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
         {
-            ViewBag.Message = "Your application description page.";
+            if(string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName)|| string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
-            return View();
+                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
+                                        (@FirstName, @LastName, @EmailAddress)";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+
+                    command.Parameters["@FirstName"].Value = firstName;
+                    command.Parameters["@LastName"].Value = lastName;
+                    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+                    return View("Success");
+            }
         }
 
-        public ActionResult Contact()
+        public ActionResult Admin()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
