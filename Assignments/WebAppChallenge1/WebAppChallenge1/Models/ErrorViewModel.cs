3b85ffa2c3b9a8f24e2eafa085ef3dd0fed2ff1b
Tony B <tbongio27@gmail.com>
2022-02-05 12:38:01 -0700

    Updated Web Application Challenge #1: Core MVC app
    Create Web App utilizing DateTime, manipulate output to user

---
+++ Assignments/WebAppChallenge1/WebAppChallenge1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppChallenge1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
